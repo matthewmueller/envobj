5b72f7d683972b9d5b6400cda6e3a4ddd415062e
Blake Embrey <hello@blakeembrey.com>
2018-04-21 00:28:07 +0900

    Enable TypeScript conditional types

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,19 @@
+declare function envobj <T> (config: T): envobj.Result<T>
 declare function envobj <T> (config: envobj.Config<T>): T
 
 declare namespace envobj {
   type Config <T> = {
-    [K in keyof T]: string | number | boolean | typeof Number | typeof String | typeof Boolean
+    [K in keyof T]: T[K] extends string
+      ? (string | typeof String) : T[K] extends number
+      ? (number | typeof Number) : T[K] extends boolean
+      ? (boolean | typeof Boolean) : never
+  }
+
+  type Result <T> = {
+    [K in keyof T]: T[K] extends string | typeof String
+      ? string : T[K] extends number | typeof Number
+      ? number : T[K] extends boolean | typeof Boolean
+      ? boolean : never
   }
 }
 
