49bda044e1e7472fd6a5ad088bedda2a9760d124
Blake Embrey <hello@blakeembrey.com>
2018-04-21 00:45:13 +0900

    Extend valid interfaces to reject invalid types

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,15 @@
-declare function envobj <T> (config: T): envobj.Result<T>
-declare function envobj <T> (config: envobj.Config<T>): T
+declare function envobj <T extends envobj.ValidConfig> (config: T): envobj.Result<T>
+declare function envobj <T extends envobj.ValidResult> (config: envobj.Config<T>): T
 
 declare namespace envobj {
+  interface ValidConfig {
+    [key: string]: string | typeof String | number | typeof Number | boolean | typeof Boolean
+  }
+
+  interface ValidResult {
+    [key: string]: string | number | boolean
+  }
+
   type Config <T> = {
     [K in keyof T]: T[K] extends string
       ? (string | typeof String) : T[K] extends number
